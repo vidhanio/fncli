e8d35a1770c79a7516eaf9bf7c949b9ce281edbf
Vidhan Bhatt <me@vidhan.io>
2022-11-29 02:44:01 -0500

    feat: span everything

--- src/lib.rs
+++ src/lib.rs
@@ -51,7 +51,7 @@ use std::convert::identity;
 
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
-use quote::quote;
+use quote::quote_spanned;
 use syn::{
     parse::Parser, parse_macro_input::ParseMacroInput, spanned::Spanned, AttributeArgs, Error,
     ItemFn, PatType, Signature,
@@ -78,7 +78,7 @@ fn parse(attr: TokenStream2, item: TokenStream2) -> Result<TokenStream2, syn::Er
         vis,
         sig,
         block,
-    } = item;
+    } = &item;
 
     let Signature {
         constness,
@@ -92,7 +92,7 @@ fn parse(attr: TokenStream2, item: TokenStream2) -> Result<TokenStream2, syn::Er
         inputs,
         variadic,
         output,
-    } = sig;
+    } = &sig;
 
     if variadic.is_some() {
         return Err(Error::new(variadic.span(), "unexpected variadic function"));
@@ -102,37 +102,76 @@ fn parse(attr: TokenStream2, item: TokenStream2) -> Result<TokenStream2, syn::Er
         syn::FnArg::Receiver(_) => {
             Error::new(arg.span(), "unexpected `self` argument").to_compile_error()
         }
-        syn::FnArg::Typed(PatType { pat, .. }) => quote! { #pat },
+        syn::FnArg::Typed(PatType { pat, .. }) => quote_spanned!(pat.span()=> #pat),
     });
 
-    let tuple = quote! {
-        (#(#arg_patterns),*)
-    };
+    let tuple = quote_spanned!(inputs.span()=> (#(#arg_patterns),*));
 
-    let args = inputs.iter().map(|arg| {
-            match arg {
-                syn::FnArg::Receiver(r) => {
-                    Error::new(r.span(), "unexpected `self` argument").to_compile_error()
-                }
-                syn::FnArg::Typed(pt) => {
-                    let pat = &pt.pat;
-                    let ty = &pt.ty;
-                    quote! {
-                        {
-                            let arg = args.next().expect(::std::concat!("missing argument", " ", "`", stringify!(#pat), "`"));
-                            <#ty as ::std::str::FromStr>::from_str(&arg).expect(::std::concat!("failed to parse argument", " ", "`", stringify!(#pat), "`"))
-                        }
-                    }
+    let args = inputs.iter().map(|arg| match arg {
+        syn::FnArg::Receiver(r) => {
+            Error::new(r.span(), "unexpected `self` argument").to_compile_error()
+        }
+        syn::FnArg::Typed(pt) => {
+            let pat = &pt.pat;
+            let ty = &pt.ty;
+            quote_spanned! {arg.span()=>
+                {
+                    let arg = args.next().expect(
+                        ::std::concat!(
+                            "missing argument",
+                            " ",
+                            "`",
+                            stringify!(#pat),
+                            ":",
+                            " ",
+                            stringify!(#ty),
+                            "`"
+                        )
+                    );
+
+                    <#ty as ::std::str::FromStr>::from_str(&arg).expect(
+                        ::std::concat!(
+                            "failed to parse argument",
+                            " ",
+                            "`",
+                            stringify!(#pat),
+                            ":",
+                            " ",
+                            stringify!(#ty),
+                            "`",
+                        )
+                    )
                 }
             }
-        });
+        }
+    });
 
-    Ok(quote! {
+    let len = inputs.len();
+
+    Ok(quote_spanned! {item.span() =>
         #(#attrs)*
         #vis #constness #asyncness #unsafety #abi #fn_token #ident #generics() #output {
+            #[allow(clippy::let_unit_value)]
+            #[allow(unused_parens)]
             let #tuple = {
+                use ::std::iter::Iterator;
+
                 let mut args = ::std::env::args().skip(1);
-                (#(#args),*)
+
+                let tuple = (#(#args),*);
+
+                if args.next().is_some() {
+                    ::std::panic!(::std::concat!(
+                        "too many arguments",
+                        " ",
+                        "(expected",
+                        " ",
+                        #len,
+                        " arguments)",
+                    ));
+                }
+
+                tuple
             };
 
             #block
@@ -142,6 +181,8 @@ fn parse(attr: TokenStream2, item: TokenStream2) -> Result<TokenStream2, syn::Er
 
 #[cfg(test)]
 mod tests {
+    use quote::quote;
+
     use super::*;
 
     #[test]
